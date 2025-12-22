94e82047d5f6afa599dcc30fbbc9a857b04c0248
tbdavid2019 <tbdavid2019@gmail.com>
2025-12-22 15:31:05 +0800

    公司識別

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,36 @@
 <template>
-  <main class="flex flex-col justify-center">
-    <HomeTwitter />
-    <HomeHero />
-    <HomeFeatures />
-    <HomeLogos />
-    <HomeCta />
+  <main class="flex min-h-screen items-center justify-center bg-gray-50 px-4 dark:bg-gray-900">
+    <div class="max-w-2xl rounded-lg bg-white p-8 shadow-lg dark:bg-gray-800">
+      <div class="mb-6 text-center">
+        <h1 class="mb-4 text-3xl font-bold text-gray-900 dark:text-white">
+          創造智能科技股份有限公司
+        </h1>
+        <p class="text-gray-600 dark:text-gray-400">
+          創造智能科技股份有限公司的統一編號（統編）是 90510433。這家公司由高明慧擔任代表人，地址在台北市內湖區舊宗路一段159號9樓。
+        </p>
+      </div>
+      
+      <div class="space-y-4 border-t pt-6 dark:border-gray-700">
+        <div class="flex justify-between border-b pb-3 dark:border-gray-700">
+          <span class="font-semibold text-gray-700 dark:text-gray-300">統一編號:</span>
+          <span class="text-gray-900 dark:text-white">90510433</span>
+        </div>
+        
+        <div class="flex justify-between border-b pb-3 dark:border-gray-700">
+          <span class="font-semibold text-gray-700 dark:text-gray-300">公司名稱:</span>
+          <span class="text-gray-900 dark:text-white">創造智能科技股份有限公司</span>
+        </div>
+        
+        <div class="flex justify-between border-b pb-3 dark:border-gray-700">
+          <span class="font-semibold text-gray-700 dark:text-gray-300">代表人:</span>
+          <span class="text-gray-900 dark:text-white">高明慧</span>
+        </div>
+        
+        <div class="flex justify-between">
+          <span class="font-semibold text-gray-700 dark:text-gray-300">地址:</span>
+          <span class="text-right text-gray-900 dark:text-white">台北市內湖區舊宗路1段159號9樓</span>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
